85516ef2fb55e1bec62a5fe4b4b2770d0eb6adf0
Michał Murawski <mmurawski777@gmail.com>
2022-07-15 19:26:15 +0200

    Added GHA release pipeline, tagged new release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_dependencies():
 
 setup(
     name='ovds_utils',
-    version='0.1.7',
+    version='0.1.8',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="Utilities package for Open VDS.",
