cdd7539484c4bd72d84b48ae5a67c1ee1cc38609
Michał Murawski <mmurawski777@gmail.com>
2022-07-19 11:57:30 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_dependencies():
 
 setup(
     name='ovds_utils',
-    version='0.1.8',
+    version='0.1.9',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="Utilities package for Open VDS.",
