3ec3dc5d6437df9af68b96aac24b48d1c813d4af
Michał Murawski <mmurawski777@gmail.com>
2023-04-18 12:58:47 +0200

    Bumped version tag

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_dependencies():
 
 setup(
     name='ovds_utils',
-    version='0.2.10',
+    version='0.2.11',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="Utilities package for Open VDS.",
